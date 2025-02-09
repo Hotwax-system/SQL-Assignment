82632e87235a70eb617d0c855e7ffc8b2e5fca5b
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-09 17:18:41 +0530

    question 1 changes

--- sql-assignment.sql
+++ sql-assignment.sql
@@ -5,8 +5,8 @@ SELECT
     c.info_string AS email,
     t.contact_number AS phone
 FROM person p
-LEFT JOIN party_contact_mech pcm ON pcm.party_id = p.party_id
-LEFT JOIN contact_mech c ON c.contact_mech_id = pcm.contact_mech_id
+JOIN party_contact_mech pcm ON pcm.party_id = p.party_id
+JOIN contact_mech c ON c.contact_mech_id = pcm.contact_mech_id
 LEFT JOIN telecom_number t ON t.contact_mech_id = c.contact_mech_id 
 JOIN party pa on pa.party_id=p.party_id JOIN party_role pr on pr.party_id=p.party_id
 where pa.created_date >='2023-06-01' and pa.created_date <='2023-07-01' and pr.role_type_id='CUSTOMER';
