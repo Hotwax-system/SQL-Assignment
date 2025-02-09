1ad0233eaef6fbadd0736de4f29e9e5e1214855a
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-09 18:45:35 +0530

    question 3

--- sql-assignment-3.sql
+++ sql-assignment-3.sql
@@ -1,3 +1,3 @@
 SELECT p.product_id,p.internal_name,p.product_type_id,gi.id_value as NetSuitId FROM 
-product p join good_identification gi on 
-p.product_id=gi.product_id where gi.id_value is null OR gi.id_value='';
+product p left join good_identification gi on 
+p.product_id=gi.product_id where gi.id_value is null OR gi.id_value='' and gi.good_identification_type_id='ERP_ID';
