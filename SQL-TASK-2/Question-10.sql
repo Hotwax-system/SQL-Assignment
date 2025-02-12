34a470ff6ac84e5d97a4b7669072c3486c99a6b8
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-12 18:48:00 +0530

    Changes in question number 10 task 2

--- SQL-TASK-2/Question-10.sql
+++ SQL-TASK-2/Question-10.sql
@@ -1,3 +1,5 @@
-SELECT oh.sales_channel_enum_id,count(oh.order_id),oh.grand_total,oh.Entry_date
+SELECT oh.sales_channel_enum_id,
+	   count(oh.order_id) as TOTAL_ORDERS,
+       oh.grand_total,oh.Entry_date as REPORTING_PERIOD
 FROM order_header oh
 Group By oh.order_id;
