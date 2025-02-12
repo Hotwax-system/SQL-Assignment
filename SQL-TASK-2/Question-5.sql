c3782bb6f978c4d4b2cdc7d4d5d5d39a793be964
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-12 15:02:12 +0530

    Question 6 task 2

--- SQL-TASK-2/Question-5.sql
+++ SQL-TASK-2/Question-5.sql
@@ -1,17 +1,9 @@
-SELECT 
-    oi.product_id,
-    iid.inventory_item_id,
-    iid.reason_enum_id,
-    iid.effective_date,
-    oisg.facility_id,
-    SUM(oi.quantity) AS total_quantity
-FROM order_item oi 
-JOIN order_item_ship_group oisg ON oisg.order_id = oi.order_id 
-JOIN inventory_item_detail iid ON iid.order_id = oi.order_id
-WHERE iid.reason_enum_id IS NOT NULL
-GROUP BY 
-    oi.product_id, 
-    iid.inventory_item_id, 
-    iid.reason_enum_id, 
-    iid.effective_date, 
-    oisg.facility_id;
+SELECT p.product_id,
+ii.inventory_item_id,
+iid.reason_enum_id,
+ii.facility_id,
+count(p.product_id) as product_total
+FROM product p JOIN inventory_item ii ON ii.PRODUCT_ID=p.PRODUCT_ID
+JOIN inventory_item_detail iid ON iid.INVENTORY_ITEM_ID=ii.INVENTORY_ITEM_ID
+WHERE iid.reason_enum_id is not null
+Group By ii.facility_id,iid.INVENTORY_ITEM_ID,p.product_id,iid.REASON_ENUM_ID;
---
+++ SQL-TASK-2/Question-6.sql
@@ -0,0 +1,10 @@
+SELECT p.product_id,
+	   p.product_name,
+       ii.facility_id,
+       ii.QUANTITY_ON_HAND_TOTAL,
+       ii.available_to_promise_total,
+       pf.minimum_stock
+FROM product p 
+JOIN inventory_item ii ON ii.product_id=p.product_id
+JOIN product_facility pf ON pf.product_id=p.product_id
+WHERE pf.minimum_stock > ii.QUANTITY_ON_HAND_TOTAL;
