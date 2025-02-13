280cb6fa4a24e5150cf429a4acf8d62e33f737f1
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-13 16:00:58 +0530

    Queation 6 task 3

---
+++ SQL-TASK-3/Question-6.sql
@@ -0,0 +1,12 @@
+SELECT ri.order_id,
+	   ri.return_id,
+       rh.return_date,
+       ri.return_reason_id,
+       ri.return_quantity
+FROM return_item ri
+JOIN return_header rh ON rh.return_id=ri.return_id
+WHERE ri.order_id in(
+SELECT order_id FROM return_item GROUP by order_id having count(order_id)>1
+)
+ORDER BY ri.order_id,rh.return_date;
+
