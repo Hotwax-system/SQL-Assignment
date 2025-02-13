2602217315ebd031c5f4def800008d54d4fd8e95
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-13 12:24:20 +0530

    Question 2task 3

---
+++ SQL-TASK-3/Question-2.sql
@@ -0,0 +1,19 @@
+SELECT ri.return_id,
+	   oi.order_id,
+       oh.product_store_id,
+       (SELECT os.status_datetime FROM order_status os WHERE os.order_id=oi.order_id
+       ORDER BY os.status_datetime desc limit 1) as status_datetime,
+       oh.order_name,
+       rh.from_party_id,
+       rh.entry_date,
+       rh.return_date,
+       rh.return_channel_enum_id
+FROM order_item oi
+JOIN order_header oh ON oh.order_id=oi.order_id
+JOIN return_item ri ON oi.order_id=ri.order_id
+JOIN return_header rh ON rh.return_id=ri.order_id
+JOIN order_status os ON os.order_id=oi.order_id;
+
+
+
+
