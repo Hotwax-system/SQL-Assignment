83b0ddba247f41cd5137828a6c6e7366114d031d
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 16:13:15 +0530

    Update sql-assignment-2.sql

--- SQL-TASK-1/sql-assignment-2.sql
+++ SQL-TASK-1/sql-assignment-2.sql
@@ -1,1 +1,7 @@
-SELECT p.product_id,p.product_type_id,p.internal_name FROM product p JOIN product_type pt on p.product_type_id=pt.product_type_id where pt.is_physical='Y';	
+SELECT 
+  p.product_id,
+  p.product_type_id,
+  p.internal_name 
+FROM product p 
+JOIN product_type pt on p.product_type_id=pt.product_type_id 
+where pt.is_physical='Y';	
