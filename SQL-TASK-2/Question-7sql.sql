78c02e2fa25bd0540bf3eb4eec3e7bad83bf4bb8
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 20:50:30 +0530

    Update Question-7sql.sql

--- SQL-TASK-2/Question-7sql.sql
+++ SQL-TASK-2/Question-7sql.sql
@@ -1,15 +1,10 @@
 SELECT oi.order_id,
-	   oi.status_id as order_status,
-       pf.facility_id,
-	   f.facility_name,
+       oi.status_id AS order_status,
+       ii.facility_id,
+       f.facility_name,
        ft.parent_type_id
 FROM order_item oi
-JOIN product_facility pf ON pf.product_id=oi.product_id
-JOIN facility f ON pf.facility_id=f.facility_id
-JOIN facility_type ft ON ft.facility_type_id=f.facility_type_id;
-
-
-
-desc facility_type;
-	
-    
+JOIN order_item_ship_grp_inv_res oisgir ON oisgir.order_id = oi.order_id AND oi.ORDER_ITEM_SEQ_ID = oisgir.ORDER_ITEM_SEQ_ID
+JOIN inventory_item ii ON ii.INVENTORY_ITEM_ID = oisgir.INVENTORY_ITEM_ID
+JOIN facility f ON ii.facility_id = f.facility_id
+JOIN facility_type ft ON ft.facility_type_id = f.facility_type_id;
