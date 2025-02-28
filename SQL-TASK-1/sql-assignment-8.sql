e82359682ec650b848723cbf535e382e9ea60643
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 18:07:29 +0530

    Update sql-assignment-8.sql

--- SQL-TASK-1/sql-assignment-8.sql
+++ SQL-TASK-1/sql-assignment-8.sql
@@ -1,16 +1,15 @@
-select oh.order_id,
-oh.status_id as order_status,
-opp.status_id as payment_status,
-s.status_id
+select 
+  oh.order_id,
+  oh.status_id as order_status,
+  opp.status_id as payment_status,
+  s.status_id as shipment_status
 FROM order_header oh 
 JOIN order_payment_preference opp
 ON opp.order_id=oh.order_id
-JOIN item_issuance ii 
-ON ii.order_id=oh.order_id 
+JOIN order_shipment os 
+ON os.order_id=oh.order_id 
 JOIN shipment s
-ON ii.shipment_id = s.shipment_id
+ON os.shipment_id = s.shipment_id
 WHERE opp.status_id ='PAYMENT_SETTLED'
 AND s.status_id != 'SHIPMENT_SHIPPED';
-
-
 
