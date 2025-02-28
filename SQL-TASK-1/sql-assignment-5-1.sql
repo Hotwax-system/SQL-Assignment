8b3e67f274e033a01b4a027fce44361eca1ad40d
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 19:40:45 +0530

    Update sql-assignment-5-1.sql

--- SQL-TASK-1/sql-assignment-5-1.sql
+++ SQL-TASK-1/sql-assignment-5-1.sql
@@ -1,17 +1,7 @@
-SELECT 
-    oi.product_id, 
-    p.product_type_id, 
-    p.internal_name, 
-    p.facility_id,
-    oh.order_history_id, 
-    oh.order_item_seq_id, 
-    oh.ship_group_seq_id,
-    orh.status_id, 
-    oi.order_id, 
-    orh.external_id,
-    orh.product_store_id
-FROM order_item oi 
-JOIN product p ON oi.product_id = p.product_id 
-JOIN order_header orh ON oi.order_id = orh.order_id 
-JOIN order_history oh ON oi.order_id = oh.order_id
-WHERE orh.STATUS_ID='ORDER_COMPLETED' AND oh.CREATED_DATE >='2023-08-01' AND oh.CREATED_DATE<='2023-09-01';
+select 
+    HOUR(oh.order_date)+1 as Hours,
+    count(oh.order_id) as total_order
+from order_header oh 
+where oh.status_id='ORDER_COMPLETED'
+group by Hours
+order by Hours;
