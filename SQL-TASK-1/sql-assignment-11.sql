3a46826d049e331fdd3db2e00072da7ae28a06d0
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 18:57:06 +0530

    Update sql-assignment-11.sql

--- SQL-TASK-1/sql-assignment-11.sql
+++ SQL-TASK-1/sql-assignment-11.sql
@@ -1,7 +1,5 @@
-SELECT * FROM product_facility;
-SELECT * FROM facility_type;
-
-SELECT p.product_id,pf.facility_id,ft.facility_type_id,pf.minimum_stock
-FROM product p 
-JOIN product_facility pf on pf.product_id=p.product_id 
-JOIN facility_type ft on p.product_id=ft.product_id
+select 
+	distinct p.product_id,
+  pf.minimum_stock as Threshold
+from product p 
+join product_facility pf on p.product_id=pf.product_id;
