2422a2d712453148c56da8d0cd61e991a28aec5f
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-02-28 20:21:31 +0530

    Update Question-5.sql

--- SQL-TASK-2/Question-5.sql
+++ SQL-TASK-2/Question-5.sql
@@ -5,5 +5,5 @@ ii.facility_id,
 count(p.product_id) as product_total
 FROM product p JOIN inventory_item ii ON ii.PRODUCT_ID=p.PRODUCT_ID
 JOIN inventory_item_detail iid ON iid.INVENTORY_ITEM_ID=ii.INVENTORY_ITEM_ID
-WHERE iid.reason_enum_id is not null
+WHERE iid.reason_enum_id is not null and iid.reason_enum_id='VAR_LOST' OR iid.reason_enum_id='REJ_RSN_DAMAGED'
 Group By ii.facility_id,iid.INVENTORY_ITEM_ID,p.product_id,iid.REASON_ENUM_ID;
