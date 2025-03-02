ac2a8ebc3d56657006fe081fd5ddb1eade7300a8
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-02 14:57:11 +0530

    Update Question-3.sql

--- SQL-TASK-3/Question-3.sql
+++ SQL-TASK-3/Question-3.sql
@@ -1,7 +1,7 @@
-SELECT distinct(per.first_name),
-	   rh.FROM_PARTY_ID
+SELECT per.first_name,
+       rh.FROM_PARTY_ID
 FROM return_item ri
 JOIN return_header rh ON ri.return_id=rh.return_id
 JOIN person per ON per.party_id=rh.FROM_PARTY_ID
-Group BY ri.order_id,rh.return_date,per.first_name,rh.FROM_PARTY_ID
-HAVING count(ri.order_id)=1 AND month(rh.return_date)=month(current_date)-1 AND rh.FROM_PARTY_ID != '_NA_';
+WHERE ri.return_id IN(select return_id FROM return_item GROUP BY order_id HAVING count(*)=1)
+AND rh.FROM_PARTY_ID != '_NA_';
