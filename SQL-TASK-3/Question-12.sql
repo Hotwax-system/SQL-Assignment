59faff1d954a454fb236a3b85ef87ecc7de881c4
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-02 16:32:58 +0530

    Update Question-12.sql

--- SQL-TASK-3/Question-12.sql
+++ SQL-TASK-3/Question-12.sql
@@ -5,6 +5,6 @@ SELECT oh.order_id,
        datediff(DATE(oh.entry_date),DATE(oh.order_date)) AS DURATION
 FROM order_header oh 
 JOIN order_item_ship_group oisg ON oh.order_id=oisg.order_id
-JOIN picklist pl ON pl.facility_id=oisg.facility_id 
-WHERE pl.status_id is null;
+LEFT JOIN picklist pl ON pl.facility_id=oisg.facility_id 
+WHERE pl.picklist_Id is null;
        
