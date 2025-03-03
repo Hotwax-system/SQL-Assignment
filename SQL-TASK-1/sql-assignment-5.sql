3c602368ee968c3f4b6ef3fc8cdfb7677aa9e52c
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-03 13:09:59 +0530

    Update sql-assignment-5.sql

--- SQL-TASK-1/sql-assignment-5.sql
+++ SQL-TASK-1/sql-assignment-5.sql
@@ -1,8 +1,9 @@
 SELECT 
-    oi.product_id, 
+    distinct oi.product_id, 
     p.product_type_id, 
     p.internal_name, 
-    p.facility_id,
+    pf.facility_id,
+    f.facility_type_id,
     oh.order_history_id, 
     oh.order_item_seq_id, 
     oh.ship_group_seq_id,
@@ -13,6 +14,10 @@ SELECT
     orh.product_store_id
 FROM order_item oi 
 JOIN product p ON oi.product_id = p.product_id 
-JOIN order_header orh ON oi.order_id = orh.order_id 
+JOIN order_item_ship_group oisg ON oisg.order_id=oi.order_id
+JOIN order_header orh ON oi.order_id = orh.order_id
+JOIN product_facility pf ON pf.product_id=oi.product_id AND oisg.facility_id=pf.facility_id
+JOIN facility f ON f.facility_id=oisg.facility_id
 JOIN order_history oh ON oi.order_id = oh.order_id
 WHERE orh.STATUS_ID='ORDER_COMPLETED' AND oh.CREATED_DATE >='2023-08-01' AND oh.CREATED_DATE<='2023-09-01';
+
