2e6930fb9673ba3e2e46459255cae652117c9f92
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-03 14:40:57 +0530

    Update sql-assignment-10.sql

--- SQL-TASK-1/sql-assignment-10.sql
+++ SQL-TASK-1/sql-assignment-10.sql
@@ -3,5 +3,4 @@ SELECT
   SUM(oh.grand_total) as Total_revenue
 FROM order_header oh 
 JOIN order_item_ship_group oisg ON oh.order_id=oisg.order_id 
-WHERE oisg.shipment_method_type_id='STOREPICKUP' AND YEAR(oh.order_date)=YEAR(current_date())-1
-GROUP BY YEAR(oh.order_date);
+WHERE oisg.shipment_method_type_id='STOREPICKUP' AND YEAR(oh.order_date)=YEAR(current_date())-1;
