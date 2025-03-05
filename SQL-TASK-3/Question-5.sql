da63ed768c02a7f24447c2781e98174ded8785e0
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 19:53:29 +0530

    Update Question-5.sql

--- SQL-TASK-3/Question-5.sql
+++ SQL-TASK-3/Question-5.sql
@@ -1,15 +1,13 @@
 SELECT distinct (ri.return_id) as Return_id,
+	   rh.entry_date,
        ra.return_adjustment_type_id,
        ra.amount,
        ra.comments,
        ri.order_id,
        oh.order_date,
-       oh.product_store_id,
        rh.return_date,
-       rh.entry_date
+       oh.product_store_id
 FROM return_item ri
-JOIN return_adjustment ra ON ra.return_id=ri.return_id
-JOIN return_header rh ON rh.return_id=ri.return_id
-JOIN order_header oh ON oh.order_id=ri.order_id;
-
-
+JOIN return_adjustment ra ON ra.return_id = ri.return_id
+JOIN return_header rh ON rh.return_id = ri.return_id
+JOIN order_header oh ON oh.order_id = ri.order_id;
