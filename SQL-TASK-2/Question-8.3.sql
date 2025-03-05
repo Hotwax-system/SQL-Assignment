28cdbaac7e10ddd92d5f2ad48a0e3d659496e4e0
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 17:08:19 +0530

    Update Question-8.3.sql

--- SQL-TASK-2/Question-8.3.sql
+++ SQL-TASK-2/Question-8.3.sql
@@ -1,10 +1,10 @@
-SELECT oi.order_id,
-       oi.status_id AS order_status,
-       ii.facility_id,
+SELECT oh.order_id,
+       oh.status_id AS order_status,
+       oisg.facility_id,
        f.facility_name,
        ft.parent_type_id
-FROM order_item oi
-JOIN order_item_ship_grp_inv_res oisgir ON oisgir.order_id = oi.order_id AND oi.ORDER_ITEM_SEQ_ID = oisgir.ORDER_ITEM_SEQ_ID
-JOIN inventory_item ii ON ii.INVENTORY_ITEM_ID = oisgir.INVENTORY_ITEM_ID
-JOIN facility f ON ii.facility_id = f.facility_id
-JOIN facility_type ft ON ft.facility_type_id = f.facility_type_id;
+FROM order_header oh
+JOIN order_item_ship_group oisg ON oisg.order_id = oh.order_id
+JOIN facility f ON oisg.facility_id = f.facility_id
+JOIN facility_type ft ON ft.facility_type_id = f.facility_type_id
+WHERE oh.status_id !='ORDER_COMPLETED' OR oh.status_id!='ORDER_CANCELLED';
