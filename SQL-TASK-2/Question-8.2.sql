aaf6cfbc5684945383ba14afa093ee93d758a358
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 16:49:30 +0530

    Update Question-8.2.sql

--- SQL-TASK-2/Question-8.2.sql
+++ SQL-TASK-2/Question-8.2.sql
@@ -1,9 +1,10 @@
-SELECT p.product_id,
-	   p.product_name,
+SELECT  
+	p.product_id,
+	p.product_name,
        ii.facility_id,
        ii.QUANTITY_ON_HAND_TOTAL,
        ii.available_to_promise_total,
-       pf.minimum_stock
+       pf.minimum_stock as REORDER_THRESHOLD
 FROM product p 
 JOIN inventory_item ii ON ii.product_id=p.product_id
 JOIN product_facility pf ON pf.product_id=p.product_id
