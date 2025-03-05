2b26969b5ef3cc274c523a6d7d9fd998e790fcb4
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 11:31:01 +0530

    Update sql-assignment-2.sql

--- SQL-TASK-1/sql-assignment-2.sql
+++ SQL-TASK-1/sql-assignment-2.sql
@@ -5,4 +5,4 @@ SELECT
 FROM product p 
 JOIN product_type pt on p.product_type_id=pt.product_type_id 
 where pt.is_physical='Y'
-and p.sales_discontinuation_date is not null;	
+and p.sales_discontinuation_date is null;	
