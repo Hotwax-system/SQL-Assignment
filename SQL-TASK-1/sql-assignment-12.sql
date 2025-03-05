bedd51ddfe3399c9b72ebaf8ab99bcdeb80ff53f
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 16:39:02 +0530

    Update sql-assignment-12.sql

--- SQL-TASK-1/sql-assignment-12.sql
+++ SQL-TASK-1/sql-assignment-12.sql
@@ -3,5 +3,6 @@ select
   pf.minimum_stock as Threshold
 from product p 
 join product_facility pf on p.product_id=pf.product_id 
-WHERE pf.minimum_stock is not null
+join facility f ON f.facility_id=pf.facility_id
+WHERE facility_type_id='CONFIGURATION'
 order by pf.minimum_stock desc;
