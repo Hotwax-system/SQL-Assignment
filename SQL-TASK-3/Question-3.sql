5e4bb812cd9a885ac6164b7226d00c7ca3f739ba
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 19:42:59 +0530

    Update Question-3.sql

--- SQL-TASK-3/Question-3.sql
+++ SQL-TASK-3/Question-3.sql
@@ -3,5 +3,5 @@ SELECT per.first_name,
 FROM return_item ri
 JOIN return_header rh ON ri.return_id=rh.return_id
 JOIN person per ON per.party_id=rh.FROM_PARTY_ID
-WHERE ri.return_id IN(select return_id FROM return_item GROUP BY order_id HAVING count(*)=1)
-AND rh.FROM_PARTY_ID != '_NA_';
+WHERE ri.order_id IN(select order_id FROM return_item GROUP BY order_id HAVING count(*)=1)
+order by rh.FROM_PARTY_ID;
