83af4d3dfd9e7027426882d5f4a96fdb7394d314
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 14:16:08 +0530

    Update sql-assignment-7.sql

--- SQL-TASK-1/sql-assignment-7.sql
+++ SQL-TASK-1/sql-assignment-7.sql
@@ -13,10 +13,8 @@ SELECT
   oh.order_id,
   oh.grand_total,
   opp.payment_method_type_id,
-  sso.shopify_order_id 
+  oh.external_id as shopify_order_id
 FROM order_header oh 
 JOIN order_payment_preference opp 
 ON opp.order_id=oh.order_id 
-JOIN shopify_shop_order sso
-ON oh.order_id=sso.order_id
 WHERE oh.status_id='ORDER_CREATED';
