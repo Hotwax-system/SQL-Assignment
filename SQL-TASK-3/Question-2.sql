2a33bb27b4b2a84f8e58be8ab46292a26448d401
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 19:07:36 +0530

    Update Question-2.sql

--- SQL-TASK-3/Question-2.sql
+++ SQL-TASK-3/Question-2.sql
@@ -5,9 +5,10 @@ SELECT ri.return_id,
        ORDER BY os.status_datetime desc limit 1) as status_datetime,
        oh.order_name,
        rh.from_party_id,
-       rh.entry_date,
        rh.return_date,
+       rh.entry_date,
        rh.return_channel_enum_id
-FROM  return_item ri
+FROM return_header rh
+JOIN return_item ri ON rh.return_id=ri.return_id 
 JOIN order_header oh ON oh.order_id=ri.order_id
-JOIN return_header rh ON rh.return_id=ri.return_id;
+where ri.status_id='RETURN_COMPLETED';
