785aa7c8d98ee3d5caf2db4ffb44567ea3333cab
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 17:56:12 +0530

    Update Question-8.4.sql

--- SQL-TASK-2/Question-8.4.sql
+++ SQL-TASK-2/Question-8.4.sql
@@ -1,8 +1,7 @@
 SELECT 
-       p.product_id,
-       p.product_name,
        ii.facility_id,
        ii.QUANTITY_ON_HAND_TOTAL,
        ii.available_to_promise_total,
-       (ii.QaUANTITY_ON_HAND_TOTAL-ii.available_to_promise_total) as Difference
-FROM inventory_item ii JOIN Product p ON ii.product_id=p.product_id;
+       (ii.QUANTITY_ON_HAND_TOTAL-ii.available_to_promise_total) as Difference
+FROM inventory_item ii;
+
