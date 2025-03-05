3f86159e3b12c8eb89af7ea7b5aadfcc41341b07
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 14:41:37 +0530

    Update sql-assignment-11.sql

--- SQL-TASK-1/sql-assignment-11.sql
+++ SQL-TASK-1/sql-assignment-11.sql
@@ -2,6 +2,6 @@ select
 	change_reason,
     	count(order_id) as total 
 from order_status 
-where status_id='ORDER_CANCELLED' and (status_datetime) >='2024-12-31' and date(status_datetime) <='2025-01-31' 
-and year(status_datetime)=year(current_date())
+	where status_id='ORDER_CANCELLED' AND status_datetime >= DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-%d')
+AND status_datetime < DATE_FORMAT(CURDATE(), '%Y-%m-%d')
 group by CHANGE_REASON;
