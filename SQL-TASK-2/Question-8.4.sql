c4e90a061e13547312a302f3706b8e55ff6c7886
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 17:33:15 +0530

    Update Question-8.4.sql

--- SQL-TASK-2/Question-8.4.sql
+++ SQL-TASK-2/Question-8.4.sql
@@ -1,10 +1,8 @@
-SELECT p.product_id,
-	   p.product_name,
+SELECT 
+       p.product_id,
+       p.product_name,
        ii.facility_id,
        ii.QUANTITY_ON_HAND_TOTAL,
        ii.available_to_promise_total,
-       (ii.QUANTITY_ON_HAND_TOTAL-ii.available_to_promise_total) as Difference
-FROM product p 
-JOIN inventory_item ii ON ii.product_id=p.product_id
-JOIN product_facility pf ON pf.product_id=p.product_id
-WHERE pf.minimum_stock > ii.QUANTITY_ON_HAND_TOTAL;
+       (ii.QaUANTITY_ON_HAND_TOTAL-ii.available_to_promise_total) as Difference
+FROM inventory_item ii JOIN Product p ON ii.product_id=p.product_id;
