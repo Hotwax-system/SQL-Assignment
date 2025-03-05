6e32789f6382a1c9849f8e8ce5946e9b1844f271
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 14:59:54 +0530

    Update sql-assignment-12.sql

--- SQL-TASK-1/sql-assignment-12.sql
+++ SQL-TASK-1/sql-assignment-12.sql
@@ -1,12 +1,7 @@
 select 
-	distinct p.product_id,
+  p.product_id,
   pf.minimum_stock as Threshold
 from product p 
-join product_facility pf on p.product_id=pf.product_id;
--- SELECT p.product_id,pf.minimum_stock as Threshold
--- FROM product p 
--- JOIN product_facility pf ON pf.product_id=p.product_id 
--- JOIN facility f on p.facility_id=f.facility_id 
--- JOIN facility_type ft on ft.facility_type_id=f.facility_type_id
--- WHERE ft.PARENT_TYPE_ID='VIRTUAL_FACILITY';
-
+join product_facility pf on p.product_id=pf.product_id 
+WHERE pf.minimum_stock is not null
+order by pf.minimum_stock desc;
