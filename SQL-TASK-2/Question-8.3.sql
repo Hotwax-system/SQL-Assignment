3b55faf277cb646c9b92b5c2e91f747ee8025595
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 18:03:08 +0530

    Update Question-8.3.sql

--- SQL-TASK-2/Question-8.3.sql
+++ SQL-TASK-2/Question-8.3.sql
@@ -7,4 +7,4 @@ FROM order_header oh
 JOIN order_item_ship_group oisg ON oisg.order_id = oh.order_id
 JOIN facility f ON oisg.facility_id = f.facility_id
 JOIN facility_type ft ON ft.facility_type_id = f.facility_type_id
-WHERE oh.status_id !='ORDER_COMPLETED' OR oh.status_id!='ORDER_CANCELLED';
+WHERE oh.status_id !='ORDER_COMPLETED' AND oh.status_id !='ORDER_CANCELLED';
