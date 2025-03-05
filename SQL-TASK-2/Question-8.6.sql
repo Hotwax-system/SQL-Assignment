bb881ca598bb7154884007b2cdcfba42a275b669
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 18:06:31 +0530

    Update Question-8.6.sql

--- SQL-TASK-2/Question-8.6.sql
+++ SQL-TASK-2/Question-8.6.sql
@@ -1,6 +1,6 @@
 SELECT oh.sales_channel_enum_id,
 	   count(oh.order_id) as TOTAL_ORDERS,
-       oh.grand_total as TOTAL_REVENUE,
-       oh.Entry_date as REPORTING_PERIOD
+      sum(oh.grand_total) as TOTAL_REVENUE,
+       concat(min(oh.order_date), ' to ', max(oh.order_date)) as REPORTING_PERIOD
 FROM order_header oh
-Group By oh.order_id;
+Group By oh.sales_channel_enum_id;
