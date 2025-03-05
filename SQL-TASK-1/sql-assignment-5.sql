4827c27ef55f573050b2cd1d28b9386c5ef573cb
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 14:13:23 +0530

    Update sql-assignment-5.sql

--- SQL-TASK-1/sql-assignment-5.sql
+++ SQL-TASK-1/sql-assignment-5.sql
@@ -1,23 +1,19 @@
-SELECT 
-    distinct oi.product_id, 
-    p.product_type_id, 
-    p.internal_name, 
-    pf.facility_id,
-    f.facility_type_id,
-    oh.order_history_id, 
-    oh.order_item_seq_id, 
-    oh.ship_group_seq_id,
-    orh.status_id, 
-    oi.order_id, 
-    oi.quantity,
-    orh.external_id,
-    orh.product_store_id
-FROM order_item oi 
-JOIN product p ON oi.product_id = p.product_id 
-JOIN order_item_ship_group oisg ON oisg.order_id=oi.order_id
-JOIN order_header orh ON oi.order_id = orh.order_id
-JOIN product_facility pf ON pf.product_id=oi.product_id AND oisg.facility_id=pf.facility_id
-JOIN facility f ON f.facility_id=oisg.facility_id
-JOIN order_history oh ON oi.order_id = oh.order_id
-WHERE orh.STATUS_ID='ORDER_COMPLETED' AND oh.CREATED_DATE >='2023-08-01' AND oh.CREATED_DATE<='2023-09-01';
-
+select p.product_id,
+       p.product_type_id,
+       oh.product_store_id,
+       oi.quantity AS TotalQuantity,
+       p.internal_name,
+       f.facility_id,
+       f.external_id,
+       f.facility_type_id,
+       orh.order_history_id,
+       orh.order_id,
+       orh.order_item_seq_id,
+       orh.ship_group_seq_id
+from Order_Header oh JOIN order_item oi ON oi.order_id=oh.order_id
+JOIN product p ON p.product_id=oi.product_id 
+JOIN order_history orh ON orh.order_id=oh.order_id
+JOIN order_item_ship_group oisg ON oisg.ship_group_seq_id=orh.ship_group_seq_id and oisg.order_id=oh.order_id
+JOIN facility f ON f.facility_id=oisg.facility_id 
+JOIN order_status os ON os.order_id=oh.order_id
+WHERE os.status_id='ORDER_COMPLETED' and date(os.status_datetime)>=date('2023-08-01') AND date(os.status_datetime)<=date('2023-08-31');
