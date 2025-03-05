784eb4d4f38627497723ab82015292b6b493b7fe
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-05 16:03:38 +0530

    Update Question-7.3.sql

--- SQL-TASK-2/Question-7.3.sql
+++ SQL-TASK-2/Question-7.3.sql
@@ -1,9 +1,10 @@
 SELECT f.facility_id,
 	   f.facility_name,
-       count(oi.order_id) as total_orders,
-       sum(oi.quantity * oi.unit_price) as total_revenue
-FROM order_item oi
-JOIN order_item_ship_group oisg ON oisg.order_id=oi.order_id
+       count(oh.order_id) as total_orders,
+       sum(oh.GRAND_TOTAL) as total_revenue,
+       concat(min(oh.order_date), ' to ', max(oh.order_date)) as date_range
+FROM order_header oh
+JOIN order_item_ship_group oisg ON oisg.order_id=oh.order_id
 JOIN facility f ON oisg.facility_id=f.facility_id 
-WHERE oi.status_id='ITEM_COMPLETED'
+WHERE oh.status_id='ORDER_COMPLETED'
 GROUP BY f.facility_id;
