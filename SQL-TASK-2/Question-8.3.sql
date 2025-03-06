33c43d65ce7d89c7178bc4e4f291777b76ffc511
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 16:02:41 +0530

    Update Question-8.3.sql

--- SQL-TASK-2/Question-8.3.sql
+++ SQL-TASK-2/Question-8.3.sql
@@ -1,9 +1,12 @@
-SELECT oh.order_id,
-       oh.status_id AS order_status,
-       oisg.facility_id,
-       f.facility_name,
-       f.facility_type_id
-FROM order_header oh
-JOIN order_item_ship_group oisg ON oisg.order_id = oh.order_id
-JOIN facility f ON oisg.facility_id = f.facility_id
-WHERE oh.status_id !='ORDER_COMPLETED' AND oh.status_id !='ORDER_CANCELLED';
+SELECT  
+    OH.order_id,  
+    OH.status_id AS order_status,  
+    F.facility_id,  
+    F.facility_name,  
+    FT.parent_type_id  
+FROM order_header AS OH  
+JOIN order_item_ship_group AS OISG ON OH.order_id = OISG.order_id  
+JOIN facility AS F ON OISG.facility_id = F.facility_id  
+JOIN facility_type FT ON FT.facility_type_id = f.facility_type_id
+WHERE OH.status_id NOT IN ('ORDER_COMPLETED', 'ORDER_CANCELLED')  AND FT.parent_type_id IN ("VIRTUAL_FACILITY", "PHYSICAL_STORE", "DISTRIBUTION_CENTER")
+ORDER BY OH.order_id;  
