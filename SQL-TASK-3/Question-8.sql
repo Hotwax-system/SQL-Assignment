870f1f6d36fe0bdfb8f51c7438b236bd7573c54e
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-06 10:16:23 +0530

    Update Question-8.sql

--- SQL-TASK-3/Question-8.sql
+++ SQL-TASK-3/Question-8.sql
@@ -1,15 +1,9 @@
 SELECT distinct(plr.party_id),
+	   per.first_name,
 	   plr.role_type_id,
-       per.first_name,
-       per.last_name,
        pl.facility_id,
-       CASE 
-			WHEN plr.thru_date is null THEN 'ACtive'
-            ELSE 'Inactive'
-	   END AS User_Status
+	   p.status_id as User_Status
 FROM picklist_role plr 
 JOIN person per ON per.party_id=plr.party_id
-JOIN picklist pl ON pl.picklist_id=plr.picklist_id;
-       
-       
-			
+JOIN picklist pl ON pl.picklist_id=plr.picklist_id
+JOIN party p ON p.party_id=plr.party_id;
