0e799d9f0c34fb50ea7a4e39160ea37953841e90
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-03-06 10:35:16 +0530

    Update Question-11.sql

--- SQL-TASK-3/Question-11.sql
+++ SQL-TASK-3/Question-11.sql
@@ -3,9 +3,9 @@ SELECT
   it.facility_id,
   it.facility_id_to,
   ii.product_id,
-  l.quantity,
-  ii.quantity_On_Hand_Total,
+  it.quantity as REQUESTED_QUANTITY,
+  oisgir.quantity as RESERVED_QUANTITY,
   it.send_date,
-  it.status_id
+  it.status_id 
 FROM Inventory_Transfer it JOIN inventory_item ii ON ii.inventory_item_id=it.inventory_item_id
-JOIN lot l ON l.inventory_item_id=it,inventory_item_id
+JOIN order_item_ship_grp_inv_res oisgir ON oisgir.inventory_item_id=it.inventory_item_id;
