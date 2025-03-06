5d3ec3df85a440b48c682a8c38a6a312dd20f24f
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 15:50:31 +0530

    Update Question-8.3.sql

--- SQL-TASK-2/Question-8.3.sql
+++ SQL-TASK-2/Question-8.3.sql
@@ -2,9 +2,8 @@ SELECT oh.order_id,
        oh.status_id AS order_status,
        oisg.facility_id,
        f.facility_name,
-       ft.parent_type_id
+       f.facility_type_id
 FROM order_header oh
 JOIN order_item_ship_group oisg ON oisg.order_id = oh.order_id
 JOIN facility f ON oisg.facility_id = f.facility_id
-JOIN facility_type ft ON ft.facility_type_id = f.facility_type_id
 WHERE oh.status_id !='ORDER_COMPLETED' AND oh.status_id !='ORDER_CANCELLED';
