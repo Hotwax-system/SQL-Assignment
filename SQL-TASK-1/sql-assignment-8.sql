04e0aa3f2aba65e92fe6c8bb0b3b0d31006fb4aa
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-03 19:24:07 +0530

    Update sql-assignment-8.sql

--- SQL-TASK-1/sql-assignment-8.sql
+++ SQL-TASK-1/sql-assignment-8.sql
@@ -1,12 +1,12 @@
 select 
-  oh.order_id,
+  distinct oh.order_id,
   oh.status_id as order_status,
   opp.status_id as payment_status,
   s.status_id as shipment_status
 FROM order_header oh 
 JOIN order_payment_preference opp ON opp.order_id=oh.order_id
-JOIN order_shipment os ON os.order_id=oh.order_id
-JOIN shipment s ON os.shipment_id = s.shipment_id
+LEFT JOIN order_shipment os ON os.order_id=oh.order_id
+LEFT JOIN shipment s ON os.shipment_id = s.shipment_id
 WHERE opp.status_id ='PAYMENT_SETTLED'
 AND s.status_id != 'SHIPMENT_SHIPPED'
 OR s.status_id is null;
