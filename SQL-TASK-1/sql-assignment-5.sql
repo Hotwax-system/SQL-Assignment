1913c304a893074d685a10da82f335a755361047
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-04 12:00:32 +0530

    Update sql-assignment-5.sql

--- SQL-TASK-1/sql-assignment-5.sql
+++ SQL-TASK-1/sql-assignment-5.sql
@@ -12,8 +12,8 @@ select p.product_id,
        orh.ship_group_seq_id
 from Order_Header oh JOIN order_item oi ON oi.order_id=oh.order_id
 JOIN product p ON p.product_id=oi.product_id 
-JOIN order_history orh ON orh.order_id=oh.order_id
-JOIN order_item_ship_group oisg ON oisg.ship_group_seq_id=orh.ship_group_seq_id and oisg.order_id=oh.order_id
+JOIN order_history orh ON orh.order_id=oi.order_id and oi.ORDER_ITEM_SEQ_ID = orh.ORDER_ITEM_SEQ_ID
+JOIN order_item_ship_group oisg ON oisg.ship_group_seq_id=oi.ship_group_seq_id and oisg.order_id=oi.order_id
 JOIN facility f ON f.facility_id=oisg.facility_id 
 JOIN order_status os ON os.order_id=oh.order_id
 WHERE os.status_id='ORDER_COMPLETED' and date(os.status_datetime)>=date('2023-08-01') AND date(os.status_datetime)<=date('2023-08-31');
