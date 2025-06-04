79c47d2b6d766861e60bc27949a47f6f42de69dc
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-04 16:38:54 +0530

    Update Question-8.3.sql

--- SQL-TASK-2/Question-8.3.sql
+++ SQL-TASK-2/Question-8.3.sql
@@ -1,9 +1,13 @@
-SELECT oh.order_id,
-       oh.status_id AS order_status,
+SELECT 
+	distinct oi.order_id,
+       oi.status_id AS order_status,
        oisg.facility_id,
        f.facility_name,
-       f.facility_type_id
-FROM order_header oh
-JOIN order_item_ship_group oisg ON oisg.order_id = oh.order_id
+       f.facility_type_id,
+       osh.shipment_id
+FROM order_item oi
+JOIN order_item_ship_group oisg ON oisg.order_id = oi.order_id and oisg.SHIP_GROUP_SEQ_ID=oi.SHIP_GROUP_SEQ_ID
 JOIN facility f ON oisg.facility_id = f.facility_id
-WHERE oh.status_id !='ORDER_COMPLETED' AND oh.status_id !='ORDER_CANCELLED';
+LEFT JOIN order_shipment osh on osh.order_id=oi.order_id and osh.ORDER_ITEM_SEQ_ID=oi.ORDER_ITEM_SEQ_ID
+WHERE oi.status_id = 'ITEM_APPROVED' and
+f.facility_type_id='RETAIL_STORE' and  osh.shipment_id is null;
