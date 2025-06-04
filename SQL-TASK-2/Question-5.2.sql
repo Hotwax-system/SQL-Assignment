6d085c8d46792bd27174198a50eccd8d191a9cba
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-04 12:27:49 +0530

    Update Question-5.2.sql

--- SQL-TASK-2/Question-5.2.sql
+++ SQL-TASK-2/Question-5.2.sql
@@ -1,17 +1,12 @@
-SELECT 
-oh.order_id,
-per.first_name,
-per.last_name,
-pad.address1,
-pad.address2,
-pad.city,
-pad.state_province_geo_id,
-pad.postal_code,
-pad.country_geo_id,
-oh.Entry_date 
-FROM order_header oh
-JOIN order_contact_mech ocm ON ocm.order_id=oh.order_id AND ocm.CONTACT_MECH_PURPOSE_TYPE_ID='SHIPPING_LOCATION'
-JOIN party_contact_mech pcm ON pcm.contact_mech_id=ocm.contact_mech_id
-JOIN postal_address pad ON pad.contact_mech_id=ocm.contact_mech_id
-JOIN person per ON per.party_id=pcm.party_id
-WHERE pad.state_province_geo_id='NY';
+select 
+	distinct oh.order_id,
+    pad.to_name,
+    pad.city,
+    pad.state_province_geo_id,
+    pad.postal_code,
+    oh.grand_total,
+    oh.order_date,
+    oh.status_id
+from order_header oh join order_contact_mech ocm on ocm.order_id=oh.order_id
+JOIN postal_address pad on pad.contact_mech_id = ocm.contact_mech_id 
+WHERE pad.state_province_geo_id='NY' and ocm.CONTACT_MECH_PURPOSE_TYPE_ID='SHIPPING_LOCATION';
