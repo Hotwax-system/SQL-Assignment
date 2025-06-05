60f69d947cb71dfd9d627845f99dabc70a719afb
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-05 18:40:23 +0530

    Update Question-12.sql

--- SQL-TASK-3/Question-12.sql
+++ SQL-TASK-3/Question-12.sql
@@ -1,12 +1,13 @@
-select oi.order_id,
-      oh.order_date,
-      oi.status_id,
-      oisg.facility_id,
-      concat(min(oisgir.reserved_datetime), ' to ', max(oisgir.reserved_datetime)) as duration
-from order_item oi
-JOIN order_item_ship_group oisg ON oisg.order_id = oi.order_id
-JOIN order_item_ship_grp_inv_res oisgir ON oisgir.order_id = oi.order_id and oisgir.order_item_seq_id = oi.order_item_seq_id and oisgir.ship_group_seq_id = oisg.ship_group_seq_id
-JOIN order_header oh ON oh.order_id = oi.order_id
-LEFT JOIN picklist_item pli ON pli.order_id = oi.order_id and pli.order_item_seq_id = oi.order_item_seq_id AND pli.ship_group_seq_id = oisgir.ship_group_seq_id and pli.inventory_item_id=oisgir.inventory_item_id
-WHERE pli.picklist_bin_id is null
-GROUP BY oi.order_id, oh.order_date, oi.status_id, oisg.facility_id;
+select
+	oi.order_id,
+    oh.order_date,
+    oi.status_id,
+    ii.facility_id,
+    concat(min(oisgir.reserved_datetime),' to ',max(oisgir.reserved_datetime)) as Duration
+from order_item oi JOIN order_header oh on oh.order_id=oi.order_id
+JOIN order_item_ship_grp_inv_res oisgir ON oisgir.order_id=oi.order_id and oisgir.order_item_seq_id=oi.order_item_seq_id and 
+oisgir.SHIP_GROUP_SEQ_ID = oi.SHIP_GROUP_SEQ_ID
+JOIN inventory_item ii ON ii.inventory_item_id=oisgir.inventory_item_id
+left join (select picklist_bin_id,primary_order_id,PRIMARY_SHIP_GROUP_SEQ_ID from picklist_bin ) as plb on plb.primary_order_id = oi.order_id and plb.PRIMARY_SHIP_GROUP_SEQ_ID = oi.SHIP_GROUP_SEQ_ID
+where plb.picklist_bin_id is null
+GROUP BY oi.order_id, oh.order_date, oi.status_id, ii.facility_id;
