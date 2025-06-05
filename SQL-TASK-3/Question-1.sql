d326edc5d397bfda08836daa184f8565227a615c
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-05 14:59:37 +0530

    Update Question-1.sql

--- SQL-TASK-3/Question-1.sql
+++ SQL-TASK-3/Question-1.sql
@@ -6,13 +6,15 @@ SELECT DISTINCT oi.order_id,
        oh.order_date,
        oh.entry_date,
        oh.status_id,
-	CAST(os.status_datetime AS DATE) AS status_date_time,
+	   mos.max_date as status_datetime,
        oh.order_type_id,
        oh.product_store_id
 FROM order_header oh 
 JOIN order_item oi ON oh.order_id = oi.order_id
 JOIN product p ON p.product_id = oi.product_id
-JOIN order_status os ON os.order_id = oh.order_id
+JOIN (
+SELECT max(os.status_datetime) as max_date,os.order_id from order_status os group by os.order_id
+) as mos ON mos.order_id=oi.order_id
 JOIN product_type pt ON p.product_type_id = pt.product_type_id
 WHERE pt.is_physical = 'Y' 
 AND oh.status_id = 'ORDER_COMPLETED' 
