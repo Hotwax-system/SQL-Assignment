148823eb2203d9bb1fc1a91351e61a930bfae486
Gaurav Kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2025-06-05 19:00:40 +0530

    Update Question-8.sql

--- SQL-TASK-3/Question-8.sql
+++ SQL-TASK-3/Question-8.sql
@@ -1,9 +1,14 @@
-SELECT distinct(plr.party_id),
-	   per.first_name,
-	   plr.role_type_id,
-       pl.facility_id,
-	   p.status_id as User_Status
-FROM picklist_role plr 
-JOIN person per ON per.party_id=plr.party_id
-JOIN picklist pl ON pl.picklist_id=plr.picklist_id
-JOIN party p ON p.party_id=plr.party_id;
+select
+      plr.party_id,
+      per.first_name,
+      plr.role_type_id,
+	  pli.facility_id,
+      CASE 
+		WHEN  plr.thru_date is null or plr.thru_date >current_Date()
+        THEN "Active"
+        ELSE  "Inactive"
+        END AS status
+FROM picklist pli JOIN picklist_role plr 
+ON pli.picklist_id = plr.picklist_id
+JOIN person per ON per.party_id = plr.party_id
+WHERE plr.role_type_id='WAREHOUSE_PICKER';
